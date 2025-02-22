ef2ba5acc07b2b617b18fe0891d461ddcc5266be
Parth Gandhi <parthkgandhi2912@gmail.com>
2025-02-22 17:58:03 +0530

    Created model for products table
    Signed-off-by: Parth Gandhi <parthkgandhi2912@gmail.com>

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    product_name = models.CharField(max_length=50)
+    tag_no = models.IntegerField(default=0)
+    curr_owner = models.CharField(max_length=50)
+    prev_owner = models.CharField(max_length=50, blank=True, null=True)
+    description = models.CharField(max_length=50, blank=True, null=True)
+    last_updated = models.DateTimeField(auto_now=True)
