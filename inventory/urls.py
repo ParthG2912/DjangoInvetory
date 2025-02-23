e0397ab0e53700f99f2aedb8773d5a022b88835a
Parth Gandhi <parthkgandhi2912@gmail.com>
2025-02-23 16:00:12 +0530

    Add user authentication and login/sign up templates
    Signed-off-by: Parth Gandhi <parthkgandhi2912@gmail.com>

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('product/<int:id>/', views.product, name="product"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from .models import Products
 
 # Create your views here.
+@login_required
 def home(request):
     product_list = Products.objects.order_by("product_name")
     return render(request, "home.html", {"product_list": product_list})
 
+@login_required
 def product(request, id):
     product = get_object_or_404(Products, id=id)
     return render(request, "product.html", {"product": product})
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "registration/signup.html", {"form": form})
